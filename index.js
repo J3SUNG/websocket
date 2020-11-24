aae11d2f8ef644823776d0a270f5302ab6589d70
J3SUNG <wptjd6141@naver.com>
2020-11-24 09:33:01 -0800

    chat program

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 var app = require("express")();
 var http = require("http").createServer(app);
+var io = require("socket.io")(http);
 
 app.get("/", function (req, res) {
-  res.send("Hello world!");
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", function (socket) {
+  console.log("손님 입장");
+  socket.on("chat message", function (msg) {
+    socket.broadcast.emit("chat message", msg);
+  });
+  socket.on("disconnect", function () {
+    console.log("들어올 땐 맘대로지만 나갈땐 아니란다.");
+  });
 });
 
 http.listen(3000, function () {
