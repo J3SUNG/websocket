b5452bef673d3874bcd389c0642accb247162db7
J3SUNG <wptjd6141@naver.com>
2020-11-25 06:11:06 -0800

    chat program

--- index.js
+++ index.js
@@ -2,22 +2,33 @@ var express = require("express");
 var app = express();
 var http = require("http").createServer(app);
 var io = require("socket.io")(http);
+
 var numOfUsers = 0;
 
 app.use(express.static(__dirname + "/public"));
 
 io.on("connection", function (socket) {
-  socket.on("new user", function () {
+  socket.on("new user", function (username) {
+    socket.username = username;
     ++numOfUsers;
-    socket.broadcast.emit("user joined", numOfUsers);
+    socket.broadcast.emit("user joined", {
+      username: socket.username,
+      numOfUsers: numOfUsers,
+    });
   });
 
   socket.on("chat message", function (msg) {
-    io.emit("chat message", msg);
+    io.emit("chat message", {
+      username: socket.username,
+      message: msg,
+    });
   });
   socket.on("disconnect", function () {
     --numOfUsers;
-    socket.broadcast.emit("user left", numOfUsers);
+    socket.broadcast.emit("user left", {
+      username: socket.username,
+      numOfUsers: numOfUsers,
+    });
   });
 });
 
--- public/main.js
+++ public/main.js
@@ -1,36 +1,86 @@
 $(function () {
+  var COLORS = [
+    "#e21400",
+    "#91580f",
+    "#f8a700",
+    "#f78b00",
+    "#58dc00",
+    "#287b00",
+    "#a8f07a",
+    "#4ae8c4",
+    "#3b88eb",
+    "#3824aa",
+    "#a700ff",
+    "#d300e7",
+  ];
+
   var socket = io();
 
+  var username;
+
+  var $window = $(window);
+
+  var $loginPage = $(".login.page");
+  var $chatPage = $(".chat.page");
+
+  var $usernameInput = $(".usernameInput");
   var $messages = $(".messages");
   var $inputMessage = $(".inputMessage");
+  var $currentInput = $usernameInput.focus();
 
-  socket.emit("new user", {});
+  function getUsernameColor(username) {
+    var index = 0;
+
+    for (var i = 0; i < username.length; ++i) {
+      index += username.charCodeAt(i);
+    }
+    index = Math.abs(index % COLORS.length);
+    return COLORS[index];
+  }
 
   function addMessageElement(el) {
     $messages.append(el);
   }
 
-  $("form").submit(function () {
-    socket.emit("chat message", $inputMessage.val());
-    $inputMessage.val("");
-    return false;
+  $window.keydown(function (event) {
+    if (event.which === 13) {
+      if (username) {
+        socket.emit("chat message", $inputMessage.val());
+        $inputMessage.val("");
+      } else {
+        username = $usernameInput.val();
+        if (username) {
+          $currentInput = $inputMessage.focus();
+          $loginPage.fadeOut();
+          $chatPage.show();
+          socket.emit("new user", username);
+        }
+      }
+    }
   });
 
-  socket.on("chat message", function (msg) {
-    var $message = $("<li class='message'/>").text(msg);
+  socket.on("chat message", function (data) {
+    var $usernameSpan = $('<span class="username"/>')
+      .css("color", getUsernameColor(data.username))
+      .text(data.username);
+    var $messageBodySpan = $('<span class="messageBody"/>').text(data.message);
+    var $message = $("<li class='message'/>").append(
+      $usernameSpan,
+      $messageBodySpan
+    );
     addMessageElement($message);
   });
 
-  socket.on("user joined", function (numOfUsers) {
+  socket.on("user joined", function (data) {
     var $joinLog = $("<li class='log'/>").text(
-      "손님 등장! (" + numOfUsers + ")"
+      data.username + "손님 등장! (" + data.numOfUsers + ")"
     );
     addMessageElement($joinLog);
   });
 
-  socket.on("user left", function (numOfUsers) {
+  socket.on("user left", function (data) {
     var $leftLog = $("<li class='log'/>").text(
-      "손님 퇴장! (" + numOfUsers + ")"
+      data.username + "손님 퇴장! (" + data.numOfUsers + ")"
     );
     addMessageElement($leftLog);
   });
